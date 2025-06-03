d45e5aa6ba7ef838b09045720129f3e3d3215237
Junayed5 <mdjunayed352003@gmail.com>
2025-06-03 22:05:43 +0600

    get and single get data

--- To-Do-app/server.js
+++ To-Do-app/server.js
@@ -1,45 +1,63 @@
 const http = require("http");
+const path = require("path");
+const fs = require("fs");
 
-const data = [
-    {
-        id: 1,
-        name: "junayed",
-        age: 23
-    },
-    {
-        id: 2,
-        name: "bayjid",
-        age: 23
-    },
-    {
-        id: 3,
-        name: "neser",
-        age: 23
-    },
-]
+const filePath = path.join(__dirname, "./db/todos.json");
 
 const server = http.createServer((req, res) => {
-  // console.log({req, res})
-  // res.end("Welcome to to-do app");
-  if (req.url === "/") {
+  const url = new URL(req.url, `http://${req.headers.host}`);
+  const pathName = url.pathname;
+
+  const data = fs.readFileSync(filePath, { encoding: "utf-8" });
+
+  if (pathName === "/") {
     res.end("Welcome to to-do app");
-  } else if (req.url === "/todos" && req.method === "GET") {
-    res.writeHead(200, "All Todos", {
-        // "content-type": "text/html"
-        "content-type": "application/json"
-    })
-    res.end(JSON.stringify(data));
-    // res.end(`<h1>Hello Mother fuller</h1>`);
-  } else if (req.url === "/todos/create-todo" && req.method === "POST") {
-    res.end("Todo created");
+  } else if (pathName === "/todos" && req.method === "GET") {
+    res.writeHead(200, {
+      "content-type": "application/json",
+    });
+    res.end(data);
+  } else if (pathName === "/todo" && req.method === "GET") {
+    const title = url.searchParams.get("title");
+    const data = fs.readFileSync(filePath, { encoding: "utf-8" });
+    const allTodos = JSON.parse(data);
+
+    const singleTodo = allTodos.find((todo) => todo.title === title);
+    console.log(singleTodo);
+
+    res.writeHead(200, "Single Todo", {
+      "content-type": "application/json",
+    });
+
+    res.end(JSON.stringify(singleTodo));
+  } else if (pathName === "/todos/create-todo" && req.method === "POST") {
+    let data = "";
+    req.on("data", (chunk) => {
+      data = data + chunk;
+    });
+
+    req.on("end", () => {
+      const { title, body } = JSON.parse(data);
+      const todos = fs.readFileSync(filePath, { encoding: "utf-8" });
+      const allTodos = JSON.parse(todos);
+      const createdAt = new Date().toString();
+
+      allTodos.push({ title, body, createdAt });
+
+      fs.writeFileSync(filePath, JSON.stringify(allTodos, null, 2), {
+        encoding: "utf-8",
+      });
+
+      res.end(JSON.stringify({ title, body, createdAt }, null, 2));
+    });
   } else {
-    res.statusCode = 404
+    res.statusCode = 404;
     res.end("Route not found");
   }
 });
 
 server.listen(5000, "127.0.0.1", () => {
-  console.log("✔️ Server listening port 5000");
+  console.log("✔️  Server listening port 5000");
 });
 
 /**
