24f4a6160be2d808bceea92559103a116ba0f9a2
Junayed5 <mdjunayed352003@gmail.com>
2025-06-16 16:54:20 +0600

    changed

--- module-3/dist/app.js
+++ module-3/dist/app.js
@@ -4,10 +4,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-const path_1 = __importDefault(require("path"));
 const todos_routes_1 = require("./app/todos.routes");
 const app = (0, express_1.default)();
-const filePath = path_1.default.join(__dirname, "../db/todos.json");
 // For routing
 app.use('/todos', todos_routes_1.todosRoute);
 // todosRoute act like a [app]
--- module-3/src/app.ts
+++ module-3/src/app.ts
@@ -4,14 +4,11 @@ import path from "path";
 import { todosRoute } from "./app/todos.routes";
 
 const app: Application = express();
-const filePath = path.join(__dirname, "../db/todos.json");
 
 // For routing
 app.use('/todos', todosRoute);
 
 // todosRoute act like a [app]
-
-
 app.use(express.json())
 
 app.get("/", (req: Request, res: Response) => {
